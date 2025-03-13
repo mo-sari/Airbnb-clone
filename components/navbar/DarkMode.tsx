00df1ae99199b90c03461cd8258c572026e078c7
mohsen ansari <mohsen.ansari.bim@gmail.com>
2025-03-13 17:05:23 +0330

    installed react-icons, logo component, navSearch component, changed global.css styles, providers, install next-theme, theme provider, finalized the dark mode

--- components/navbar/DarkMode.tsx
+++ components/navbar/DarkMode.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+"use client";
 
-const DarkMode = () => {
-  return <div>DarkMode</div>;
-};
+import * as React from "react";
+// import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { WiMoonAltWaningCrescent2, WiDaySunny } from "react-icons/wi";
 
-export default DarkMode;
+import { useTheme } from "next-themes";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export default function ModeToggle() {
+  const { setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <WiDaySunny className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <WiMoonAltWaningCrescent2 className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
